e9e5a5a90c0e92dda1803c222009f7a1480199db
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2025-06-02 10:03:36 +0530

    Login Validation

--- app/src/main/java/com/example/internshipbatch27may/MainActivity.java
+++ app/src/main/java/com/example/internshipbatch27may/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.internshipbatch27may;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +15,52 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText email, password;
+    TextView forget_password, create_account;
+
+    String email_pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.main_login);
+        email = findViewById(R.id.main_email);
+        password = findViewById(R.id.main_password);
+        forget_password = findViewById(R.id.main_forget_password);
+        create_account = findViewById(R.id.main_create_account);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Enter Email");
+                } else if (!email.getText().toString().matches(email_pattern)) {
+                    email.setError("Enter Valid Email");
+                } else if (password.getText().toString().trim().equals("")) {
+                    password.setError("Enter Password");
+                } else if (password.getText().toString().length()<6) {
+                    password.setError("Minimum 6 characters");
+                } else{
+                    Toast.makeText(MainActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+//                    Snackbar.make(view, "LOgin Successfull", Snackbar.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+
 
     }
 }
