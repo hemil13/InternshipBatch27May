a300d972239f3c8d19ece0342a8167f56439034d
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2025-06-23 10:39:29 +0530

    Delete Sqlite

--- app/src/main/java/com/example/internshipbatch27may/DashboardActivity.java
+++ app/src/main/java/com/example/internshipbatch27may/DashboardActivity.java
@@ -2,9 +2,11 @@ package com.example.internshipbatch27may;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +15,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DashboardActivity extends AppCompatActivity {
-    Button profile, logout, category;
+    Button profile, logout, category, delete;
+    TextView welcome;
 
     SharedPreferences sp;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        db  = openOrCreateDatabase("InternshipBatch27May.db", MODE_PRIVATE, null);
+        String userTable = "CREATE TABLE IF NOT EXISTS user(userid INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(50), email VARCHAR(100), contact VARCHAR(10), password VARCHAR(20))";
+        db.execSQL(userTable);
+
+
         sp = getSharedPreferences(ConstantSp.pref, MODE_PRIVATE);
 
         profile = findViewById(R.id.dashboard_profile);
         logout = findViewById(R.id.dashboard_logout);
         category = findViewById(R.id.dashboard_category);
+        delete = findViewById(R.id.dashboard_delete);
+        welcome = findViewById(R.id.dashboard_welcome);
+
+        welcome.setText("Welcome, "+sp.getString(ConstantSp.name,""));
 
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +66,18 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(DashboardActivity.this, CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String deleteUser = "DELETE FROM user WHERE userid = '"+sp.getString(ConstantSp.userid, "")+"'";
+                db.execSQL(deleteUser);
+                sp.edit().clear().commit();
+                Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
